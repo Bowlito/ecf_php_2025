2fa39384415b190af11e1dc2e8226801176172b3
Bowlito <julien.catoio@gmail.com>
2025-07-03 10:01:43 +0200

    creation BDD

--- ecf_db.sql
+++ ecf_db.sql
@@ -1,8 +1,53 @@
 -- Active: 1750235329113@@127.0.0.1@3306@librairie
 
-DROP DATABASE IF EXISTS librairie
+DROP DATABASE IF EXISTS librairie;
 
-CREATE DATABASE librairie
+CREATE DATABASE librairie;  
 
 USE librairie
 
+CREATE TABLE admins 
+(
+
+id INT PRIMARY KEY AUTO_INCREMENT,
+nom VARCHAR(30),
+prenom VARCHAR(30),
+mail VARCHAR(50) UNIQUE,
+mdp VARCHAR(10)
+
+);
+
+INSERT INTO admins VALUES 
+(
+
+NULL,
+'CATOIO',
+'Julien',
+'julien.catoio@ecf.com',
+'123456'
+
+);
+
+CREATE TABLE livres 
+(
+
+id INT PRIMARY KEY AUTO_INCREMENT,
+auteur_id INT,
+admin_id INT,
+added DATE,
+titre VARCHAR(30) UNIQUE,
+categories VARCHAR(30),
+annee_publication INT,
+resumes TEXT,
+Foreign Key (admin_id) REFERENCES admins(id),
+Foreign Key (auteur_id) REFERENCES auteurs(id)
+
+);
+CREATE TABLE auteurs 
+(
+
+id INT PRIMARY KEY AUTO_INCREMENT,
+nom_complet VARCHAR(50),
+nationalite VARCHAR(20)
+
+);
